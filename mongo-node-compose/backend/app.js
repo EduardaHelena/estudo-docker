4dbe2f6d8880307ebb1c17bcfc70be897e0a49ed
Eduarda Helena <dudinhahelenaa@gmail.com>
2020-05-25 11:56:44 -0300

    ajustes

--- mongo-node-compose/backend/app.js
+++ mongo-node-compose/backend/app.js
@@ -1,14 +1,14 @@
 const express = require('express')
-const restfull = require('node-restful')
-const serve = express()
-const mongooser = restfull.mongooser
-
-// Database
-mongooser.Promise = global.Promise
-mongooser.connect('mongodb:db/mydb')
-
-//Teste
-serve.get('/', (req, resp, next) => resp.send('Backend'))
-
-// Start Server
-serve.listen(3000)
+const restful = require('node-restful')
+const server = express()
+const mongoose = restful.mongoose
+ 
+//Database
+mongoose.Promise = global.Promise
+mongoose.connect('mongodb://db/mydb')
+ 
+//Teste 
+server.get('/', (req, res, next) => res.send('Backend'))
+ 
+//Start server
+server.listen(3000)
